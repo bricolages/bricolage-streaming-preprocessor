f4532a99562c8b071f1d03b12bd27749482a9c96
Shimpei Kodama <shimpei-kodama@cookpad.com>
2016-11-08 19:04:45 +0900

    Add option to preserve aggreagted columns to aggr ops

--- src/main/java/org/bricolages/streaming/filter/AggregateOp.java
+++ src/main/java/org/bricolages/streaming/filter/AggregateOp.java
@@ -1,7 +1,8 @@
 package org.bricolages.streaming.filter;
-import java.util.HashMap;
-import java.util.regex.*;
+
 import lombok.*;
+
+import java.util.regex.Pattern;
 
 class AggregateOp extends Op {
     static final void register() {
@@ -15,19 +16,22 @@ class AggregateOp extends Op {
     static class Parameters {
         String targetColumns;
         String aggregatedColumn;
+        boolean dropTargetColumns = true;
     }
 
     final Pattern targetColumns;
     final String aggregatedColumn;
+    final boolean dropTargetColumns;
 
     AggregateOp(OperatorDefinition def, Parameters params) {
-        this(def, params.targetColumns, params.aggregatedColumn);
+        this(def, params.targetColumns, params.aggregatedColumn, params.dropTargetColumns);
     }
 
-    AggregateOp(OperatorDefinition def, String targetColumns, String aggregatedColumn) {
+    AggregateOp(OperatorDefinition def, String targetColumns, String aggregatedColumn, boolean dropTargetColumns) {
         super(def);
         this.targetColumns = Pattern.compile(targetColumns);
         this.aggregatedColumn = aggregatedColumn;
+        this.dropTargetColumns = dropTargetColumns;
     }
 
     @Override
@@ -41,7 +45,9 @@ class AggregateOp extends Op {
                 if (ent.getValue() != null) {
                     buf.put(m.replaceFirst(""), ent.getValue());
                 }
-                it.remove();
+                if(dropTargetColumns) {
+                    it.remove();
+                }
             }
         }
         record.put(aggregatedColumn, buf.getObject());
--- src/test/java/org/bricolages/streaming/filter/AggregateOpTest.java
+++ src/test/java/org/bricolages/streaming/filter/AggregateOpTest.java
@@ -12,4 +12,13 @@ public class AggregateOpTest {
         val out = op.apply(rec);
         assertEquals("{\"a\":1,\"b\":4,\"q\":{\"x\":2,\"y\":3}}", out.serialize());
     }
+
+    @Test
+    public void apply_no_remove() throws Exception {
+        val def = new OperatorDefinition("aggregate", "schema.table", "*", "{\"targetColumns\":\"^q:\",\"aggregatedColumn\":\"q\",\"dropTargetColumns\":false}");
+        val op = (AggregateOp)Op.build(def);
+        val rec = Record.parse("{\"a\":1,\"q:x\":2,\"q:y\":3,\"b\":4}");
+        val out = op.apply(rec);
+        assertEquals("{\"a\":1,\"q:x\":2,\"q:y\":3,\"b\":4,\"q\":{\"x\":2,\"y\":3}}", out.serialize());
+    }
 }
