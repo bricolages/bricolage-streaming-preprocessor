82c7964ae1abb805a63aeed677afa2979f573137
Hidekazu Kobayashi <hidekazu-kobayashi@cookpad.com>
2017-02-01 13:40:42 +0900

    Throw FilterException instead of ApplicationException when allocated sequence counter is starved

--- src/main/java/org/bricolages/streaming/filter/SequenceOp.java
+++ src/main/java/org/bricolages/streaming/filter/SequenceOp.java
@@ -25,10 +25,10 @@ class SequenceOp extends SingleColumnOp {
         this.upperValue = seq.getNextValue();
     }
 
-    private long getNextValue() {
+    private long getNextValue() throws FilterException {
         currentValue ++;
         if (currentValue > upperValue) {
-            throw new ApplicationError("sequence number is starved");
+            throw new FilterException("sequence number is starved");
         }
         return currentValue;
     }
