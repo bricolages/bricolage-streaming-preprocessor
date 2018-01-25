2cba8d2e61908bfe40b0ec4dc3376aaafba319be
Minero Aoki <mineroaoki@gmail.com>
2018-01-25 19:25:18 +0900

    fix: temporary file name was wrong.

--- src/main/java/org/bricolages/streaming/stream/BoundStream.java
+++ src/main/java/org/bricolages/streaming/stream/BoundStream.java
@@ -56,6 +56,6 @@ public class BoundStream {
     }
 
     public S3ObjectMetadata processLocator(S3ObjectLocator src, S3ObjectLocator dest, FilterResult result) throws LocatorIOException, IOException, ConfigError {
-        return loadFilter().processLocator(src, dest, result, src.toString());
+        return loadFilter().processLocator(src, dest, result, stream.getStreamName());
     }
 }
