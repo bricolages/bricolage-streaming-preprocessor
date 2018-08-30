8a3eae69905f9eb378459dd004b9e10ec677a15e
Minero Aoki <mineroaoki@gmail.com>
2018-08-30 17:52:24 +0900

    fix: Select PreprocMessage first to reduce duplicated rows error

--- src/main/java/org/bricolages/streaming/preproc/PreprocMessageRepository.java
+++ src/main/java/org/bricolages/streaming/preproc/PreprocMessageRepository.java
@@ -1,11 +1,15 @@
 package org.bricolages.streaming.preproc;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.dao.DataIntegrityViolationException;
+import lombok.*;
 
 public interface PreprocMessageRepository extends JpaRepository<PreprocMessage, Long> {
     PreprocMessage findByMessageId(String msgid);
 
     default public PreprocMessage upsert(PreprocMessage msg) {
+        // to reduce duplicated rows error
+        val curr = findByMessageId(msg.getMessageId());
+        if (curr != null) return curr;
         try {
             return save(msg);
         }
