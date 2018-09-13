a6f4730f7e6808955ed420a65e948d02e1a4c617
Minero Aoki <mineroaoki@gmail.com>
2018-09-13 12:37:43 +0900

    Provides default values for strload_tables.load_batch_size, .load_interval

--- src/main/java/org/bricolages/streaming/table/TargetTable.java
+++ src/main/java/org/bricolages/streaming/table/TargetTable.java
@@ -20,6 +20,14 @@ public class TargetTable {
     @Getter
     String tableName;
 
+    @Column(name="load_batch_size")
+    @Getter
+    int loadBatchSize;
+
+    @Column(name="load_interval")
+    @Getter
+    int loadInterval;   // in seconds
+
     @Column(name="s3_bucket")
     @Getter
     String bucket;
@@ -36,5 +44,22 @@ public class TargetTable {
         this.tableName = tableName;
         this.bucket = bucket;
         this.prefix = prefix;
+        this.loadBatchSize = DEFAULT_LOAD_BATCH_SIZE;
+        this.loadInterval = randomInitialLoadInterval();
+    }
+
+    static final int DEFAULT_LOAD_BATCH_SIZE = 2400;
+
+    static final int DEFAULT_LOAD_INTERVAL_BASE = 6 * 60 * 60;   // 6 hours
+    static final int DEFAULT_LOAD_INTERVAL_MIN = (int)(DEFAULT_LOAD_INTERVAL_BASE * 0.95);
+    static final int DEFAULT_LOAD_INTERVAL_MAX = (int)(DEFAULT_LOAD_INTERVAL_BASE * 1.05);
+
+    /**
+     * Returns 6 hours +/- 5%.
+     * Fixed load interval causes too many concurrent loading, we must introduce random factor.
+     */
+    static int randomInitialLoadInterval() {
+        double randomFactor = 1 + (Math.random() - 0.5) / 10;   // [0.95, 1.05)
+        return (int)(DEFAULT_LOAD_INTERVAL_BASE * randomFactor);
     }
 }
